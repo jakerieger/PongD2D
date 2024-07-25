3da7a2c3dd2d7e8c1f88250476f1c1fdb2af8acc
Jake Rieger <contact.jakerieger@gmail.com>
2024-07-24 22:27:39 -0400

    bounding boxes

--- main.cpp
+++ main.cpp
@@ -14,10 +14,13 @@
 #pragma comment(lib, "d2d1.lib")
 #pragma comment(lib, "dwrite.lib")
 
+static constexpr int kWindowWidth  = 1200;
+static constexpr int kWindowHeight = 900;
+
 namespace Map {
     constexpr auto Values = std::ranges::views::values;
-    constexpr auto Keys = std::ranges::views::keys;
-}
+    constexpr auto Keys   = std::ranges::views::keys;
+}  // namespace Map
 
 inline void WideToANSI(const std::wstring& value, std::string& converted) {
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
@@ -31,7 +34,7 @@ inline void ANSIToWide(const std::string& value, std::wstring& converted) {
 
 class ComError final : public std::exception {
 public:
-    explicit ComError(std::string  msg) : message(std::move(msg)) {}
+    explicit ComError(std::string msg) : message(std::move(msg)) {}
 
     [[nodiscard]] const char* what() const noexcept override {
         return message.c_str();
@@ -52,6 +55,14 @@ static void CheckResult(const HRESULT hr) {
 #define dcast dynamic_cast
 #define rcast reinterpret_cast
 
+struct GameState {
+    int PlayerScore   = 0;
+    int OpponentScore = 0;
+    int ElapsedTime   = 0;
+};
+
+static GameState g_GameState = {};
+
 struct GameObject {
     D2D1_RECT_F Rect       = {};
     D2D1_COLOR_F Color     = {};
@@ -59,7 +70,7 @@ struct GameObject {
     D2D1_POINT_2F Rotation = {};
     D2D1_POINT_2F Scale    = {};
 
-    virtual void Update()                              = 0;
+    virtual void Update(double dT)                     = 0;
     virtual void Draw(ID2D1RenderTarget* renderTarget) = 0;
     virtual ~GameObject()                              = default;
 };
@@ -67,16 +78,83 @@ struct GameObject {
 static HWND g_Hwnd;
 static ID2D1Factory* g_Factory;
 static ID2D1HwndRenderTarget* g_RenderTarget;
+static IDWriteFactory* g_DWriteFactory;
 static std::unordered_map<std::string, GameObject*> g_GameObjects;
 
 struct Ball final : GameObject {
-    void Update() override {}
+    void Reset(const RECT& windowRect) {
+        m_Velocity = {300.f, 0.f};
+        Position   = D2D1::Point2F(windowRect.right / 2.f, windowRect.bottom / 2.f);
+    }
 
-    void Draw(ID2D1RenderTarget* renderTarget) override {}
+    void UpdateBoundingBox() {
+        const auto top    = Position.y - Scale.y;
+        const auto bottom = Position.y + Scale.y;
+        const auto left   = Position.x - Scale.x;
+        const auto right  = Position.x + Scale.x;
+
+        const auto boundingBox = D2D1::RectF(left, top, right, bottom);
+        this->Rect             = boundingBox;
+    }
+
+    void Move(const float dT) {
+        Position.x += m_Velocity.x * dT;
+        Position.y += m_Velocity.y * dT;
+    }
+
+    void CheckCollision() {
+        auto paddlePlayer   = g_GameObjects["Player"];
+        auto paddleOpponent = g_GameObjects["Opponent"];
+    }
+
+    void CheckOOB(const RECT& windowRect) {
+        if (Position.x < 0.0) {
+            // Score and reset ball
+            g_GameState.OpponentScore++;
+            // Reset(windowRect);
+            m_Velocity.x = -m_Velocity.x;
+        } else if (Position.x > scast<float>(windowRect.right)) {
+            // Score opponent and reset ball
+            g_GameState.PlayerScore++;
+            // Reset(windowRect);
+            m_Velocity.x = -m_Velocity.x;
+        }
+    }
+
+    void Update(const double dT) override {
+        RECT windowRect;
+        ::GetClientRect(g_Hwnd, &windowRect);
+
+        Move(scast<float>(dT));
+        UpdateBoundingBox();
+        CheckCollision();
+        CheckOOB(windowRect);
+    }
+
+    void Draw(ID2D1RenderTarget* renderTarget) override {
+        ID2D1SolidColorBrush* brush = nullptr;
+        CheckResult(renderTarget->CreateSolidColorBrush(Color, &brush));
+
+        renderTarget->FillEllipse(D2D1::Ellipse(Position, Scale.x, Scale.y), brush);
+        brush->Release();
+
+        // Draw bounding box
+        {
+            ID2D1SolidColorBrush* boundsBrush = nullptr;
+            CheckResult(
+              renderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Red), &boundsBrush));
+
+            renderTarget->DrawRectangle(Rect, boundsBrush, 1);
+            boundsBrush->Release();
+        }
+    }
+
+private:
+    D2D1_POINT_2F m_Velocity = {300.f, 0.f};
 };
 
 struct Paddle final : GameObject {
-    void Update() override {}
+    void Update(double dT) override {}
 
     void Draw(ID2D1RenderTarget* renderTarget) override {}
 };
@@ -92,6 +170,35 @@ void Initialize() {
       D2D1::HwndRenderTargetProperties(g_Hwnd, D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top)),
       &g_RenderTarget);
     CheckResult(hr);
+
+    hr = DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED,
+                             __uuidof(IDWriteFactory),
+                             rcast<IUnknown**>(&g_DWriteFactory));
+    CheckResult(hr);
+
+    {  // Initialize the game objects
+        const auto ball = new Ball;
+        ball->Color     = D2D1::ColorF(D2D1::ColorF::White);
+        ball->Position =
+          D2D1::Point2F(scast<float>(rc.right - rc.left) / 2, scast<float>(rc.bottom - rc.top) / 2);
+        ball->Scale = D2D1::Point2F(16, 16);
+
+        const auto paddlePlayer = new Paddle;
+        paddlePlayer->Color     = D2D1::ColorF(D2D1::ColorF::White);
+        paddlePlayer->Position =
+          D2D1::Point2F(scast<float>(rc.left) + 100, scast<float>(rc.bottom - rc.top) / 2);
+        paddlePlayer->Scale = D2D1::Point2F(16, 200);
+
+        const auto paddleOpponent = new Paddle;
+        paddleOpponent->Color     = D2D1::ColorF(D2D1::ColorF::White);
+        paddleOpponent->Position =
+          D2D1::Point2F(scast<float>(rc.right) - 100, scast<float>(rc.bottom - rc.top) / 2);
+        paddleOpponent->Scale = D2D1::Point2F(16, 200);
+
+        g_GameObjects["Player"]   = paddlePlayer;
+        g_GameObjects["Opponent"] = paddleOpponent;
+        g_GameObjects["Ball"]     = ball;
+    }
 }
 
 void Shutdown() {
@@ -106,9 +213,9 @@ void Shutdown() {
     }
 }
 
-void Update() {
+void Update(const double dT) {
     for (const auto& go : g_GameObjects | Map::Values) {
-        go->Update();
+        go->Update(dT);
     }
 }
 
@@ -181,6 +288,24 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     return ::DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
+namespace Timer {
+    static LARGE_INTEGER g_Frequency;
+    static LARGE_INTEGER g_LastTime;
+
+    void StartTimer() {
+        QueryPerformanceFrequency(&g_Frequency);
+        QueryPerformanceCounter(&g_LastTime);
+    }
+
+    double GetDeltaTime() {
+        LARGE_INTEGER currentTime;
+        QueryPerformanceCounter(&currentTime);
+        const LONGLONG elapsed = currentTime.QuadPart - g_LastTime.QuadPart;
+        g_LastTime             = currentTime;
+        return scast<double>(elapsed) / scast<double>(g_Frequency.QuadPart);
+    }
+}  // namespace Timer
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     HICON appIcon = ::LoadIcon(hInstance, MAKEINTRESOURCE(APPICON));
 
@@ -192,21 +317,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpfnWndProc   = WndProc;
     wc.hInstance     = hInstance;
     wc.hIcon         = appIcon;
-    wc.hIconSm        = wc.hIcon;
+    wc.hIconSm       = wc.hIcon;
     wc.hCursor       = ::LoadCursor(nullptr, IDC_ARROW);
     wc.lpszClassName = "PongWindowClass";
 
     ::RegisterClassEx(&wc);
 
+    const int scrWidth  = ::GetSystemMetrics(SM_CXSCREEN);
+    const int scrHeight = ::GetSystemMetrics(SM_CYSCREEN);
+    const int posX      = (scrWidth - kWindowWidth) / 2;
+    const int posY      = (scrHeight - kWindowHeight) / 2;
+
     // Create the window
     g_Hwnd = ::CreateWindowExA(0,
                                wc.lpszClassName,
                                "PongD2D",
-                               WS_OVERLAPPEDWINDOW,
-                               300,
-                               300,
-                               800,
-                               600,
+                               WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
+                               posX,
+                               posY,
+                               kWindowWidth,
+                               kWindowHeight,
                                nullptr,
                                nullptr,
                                hInstance,
@@ -219,8 +349,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     // Enter the main loop
     MSG msg = {};
+    Timer::StartTimer();
+
     for (;;) {
-        Update();
+        Update(Timer::GetDeltaTime());
 
         while (::PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
             ::TranslateMessage(&msg);
